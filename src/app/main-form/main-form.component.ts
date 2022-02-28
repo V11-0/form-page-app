1bfb8ab502cd434d6483c75a42f42bec5842da46
Vinicius Bruscagini <vinibrenobr11@hotmail.com>
2022-02-27 22:54:12 -0300

    Finish demo

--- src/app/main-form/main-form.component.ts
+++ src/app/main-form/main-form.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main-form',
   templateUrl: './main-form.component.html',
   styleUrls: ['./main-form.component.css']
 })
-export class MainFormComponent {
+export class MainFormComponent implements OnInit {
 
-  @ViewChild('f') appForm!: NgForm;
+  appForm!: FormGroup;
+
+  // Validators variables
+  emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+  registerFinished = false;
+
+  ngOnInit(): void {
+    this.appForm = new FormGroup({
+      'name': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, this.emailValidator.bind(this)]),
+      'password': new FormControl(null, Validators.required),
+      'passwordConfirm': new FormControl(null, [Validators.required]),
+    },
+    this.samePasswordValidator);
+  }
 
   onSubmit(): void {
-    console.log(this.appForm);
+    this.registerFinished = true;
+
+    setTimeout(() => location.reload(), 2000);
+  }
+
+  emailValidator(control: AbstractControl): ValidationErrors | null {
+    let isEmailValid = this.emailRegex.test(control.value);
+
+    return isEmailValid ? null : { isEmailInvalid: true };
+  }
+
+  samePasswordValidator(control: AbstractControl): ValidationErrors | null {
+    return control.get('password')?.value !== control.get('passwordConfirm')?.value ? { passwordsNotEqual: true } : null;
   }
 }
